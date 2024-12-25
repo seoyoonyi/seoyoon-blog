ff29dd3006ffa2ac181ed22a7b13ae472722c880
seoyoonyi <yiseoyoon@gmail.com>
2024-12-25 22:00:43 +0900

    Added functionality to toggle between dark mode and light mode

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,3 +1,4 @@
+import { ThemeSwitcher } from "app/components/theme-switcher";
 import Link from "next/link";
 
 const navItems = {
@@ -12,18 +13,18 @@ const navItems = {
 export function Navbar() {
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
+      <div className="flex justify-between lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="relative flex flex-row items-start px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row pr-10 space-x-0">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="relative flex px-2 py-1 m-1 align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
                 >
                   {name}
                 </Link>
@@ -31,6 +32,7 @@ export function Navbar() {
             })}
           </div>
         </nav>
+        <ThemeSwitcher />
       </div>
     </aside>
   );
