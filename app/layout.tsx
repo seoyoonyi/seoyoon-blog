2b3175bdc235550e8c5c2415aac268cec3d6fbb3
seoyoonyi <yiseoyoon@gmail.com>
2025-01-03 08:39:04 +0900

    update RootLayout to use Header instead of Navbar and adjust layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 
 import './global.css'
 import { baseUrl } from './sitemap'
+import Header from '@/components/common/header'
 import Footer from '@/components/footer'
-import { Navbar } from '@/components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
@@ -39,9 +39,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <head></head>
-      <body className='mx-4 mt-8 max-w-xl antialiased lg:mx-auto'>
-        <main className='mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0'>
-          <Navbar />
+      <body className='font-sans antialiased'>
+        <Header />
+        <main className='mt-6 flex min-w-0 max-w-xl flex-auto flex-col px-2 md:mx-auto md:px-0'>
           {children}
           <Footer />
           <Analytics />
