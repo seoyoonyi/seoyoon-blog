6450307a89b7d13c34711a1676b83b905a4fe4dc
seoyoonyi <yiseoyoon@gmail.com>
2025-01-03 08:32:57 +0900

    update header structure with code formatting

---
+++ components/common/header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Navbar } from '@/components/nav'
+import ThemeToggle from '@/components/theme-toggle'
+import logo from '@/public/smile.svg'
+import { Github } from 'lucide-react'
+
+const Header = () => {
+  return (
+    <header className='border-b-1 p-y-2 border px-4'>
+      <div className='my-4 flex max-w-xl items-center md:mx-auto'>
+        <div className='flex space-x-4'>
+          <Link href='/' className='flex items-center space-x-3'>
+            <Image src={logo} alt='logo' width={30} height={30} />
+            <div className='relative'>
+              <span className='block h-4 w-[1px] origin-center rotate-[17deg] bg-black dark:bg-white'></span>
+            </div>
+            <h1 className='text-2xl font-bold'>Blog</h1>
+          </Link>
+
+          <Navbar />
+        </div>
+        <div className='ml-auto flex space-x-2'>
+          <ThemeToggle />
+          <a
+            className='flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100'
+            rel='noopener noreferrer'
+            target='_blank'
+            href='https://github.com/seoyoonyi'
+          >
+            <Github size={20} />
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,39 +1,34 @@
-import ThemeToggle from "@/components/theme-toggle";
-import Link from "next/link";
+import Link from 'next/link'
 
 const navItems = {
-  "/": {
-    name: "home",
+  '/about': {
+    name: 'About',
   },
-  "/blog": {
-    name: "blog",
-  },
-};
+}
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="flex justify-between lg:sticky lg:top-20">
+    <aside className='-ml-[8px] tracking-tight'>
+      <div className='flex justify-between lg:sticky lg:top-20'>
         <nav
-          className="relative flex flex-row items-start px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
+          className='fade relative flex scroll-pr-6 flex-row items-start px-0 pb-0 md:relative md:overflow-auto'
+          id='nav'
         >
-          <div className="flex flex-row pr-10 space-x-0">
+          <div className='flex flex-row space-x-0 pr-10 pt-1'>
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="relative flex px-2 py-1 m-1 align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
+                  className='relative m-1 flex px-2 py-1 align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200'
                 >
                   {name}
                 </Link>
-              );
+              )
             })}
           </div>
         </nav>
-        <ThemeToggle />
       </div>
     </aside>
-  );
+  )
 }
