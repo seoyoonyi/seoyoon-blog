bde25346cc6e71b0fd71e1b0719d8da1cc3ae7ed
seoyoonyi <yiseoyoon@gmail.com>
2025-01-04 10:21:34 +0900

    update useCategories to match CategoryList props

--- hooks/use-categories.tsx
+++ hooks/use-categories.tsx
@@ -5,7 +5,12 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json())
 export const useCategories = () => {
   const { data, error } = useSWR('/api/categories', fetcher)
 
-  const cleanCategories = data?.categories?.filter((category) => typeof category === 'string') || []
+  const cleanCategories =
+    data?.categories?.map((item) => ({
+      dirName: item.category.toLowerCase(),
+      publicName: item.category,
+      count: item.count,
+    })) || []
 
   return {
     categories: cleanCategories,
