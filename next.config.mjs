da372d89a8662d3f0808e640b7d378647f0b1307
seoyoonyi <yiseoyoon@gmail.com>
2025-07-15 12:48:53 +0900

    integrate AWS S3 and fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,22 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "@aws-sdk/client-s3": false,
+        "@payloadcms/plugin-cloud-storage": false,
+      }
+    }
+
+    config.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
 
-    return webpackConfig
+    return config
   },
 }
 
