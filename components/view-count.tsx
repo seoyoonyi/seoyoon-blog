8e007dcd5c8a7cb28f339bb04da13e528633d99f
seoyoonyi <yiseoyoon@gmail.com>
2025-07-20 00:47:05 +0900

    refactor ViewCount to a Client Component

--- components/view-count.tsx
+++ components/view-count.tsx
@@ -1,17 +1,29 @@
-import { getViewsCount, incrementView } from "queries/db";
+'use client'
+
+import { useEffect, useState } from 'react'
+import { getViewsCount, incrementView } from 'queries/db'
 
 type Props = {
-  slug: string;
-};
+  slug: string
+}
 
-export const ViewCount = async ({ slug }: Props) => {
-  await incrementView(slug);
-  const views = await getViewsCount();
-  const count = views.find((view) => view.slug === slug)?.count || 0;
+export const ViewCount = ({ slug }: Props) => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const fetchViewCount = async () => {
+      await incrementView(slug)
+      const views = await getViewsCount()
+      const viewCount = views.find((view) => view.slug === slug)?.count || 0
+      setCount(viewCount)
+    }
+
+    fetchViewCount()
+  }, [slug])
 
   return (
     <p className="text-sm text-neutral-600 dark:text-neutral-400">
       {count.toLocaleString()} views
     </p>
-  );
-};
+  )
+}
